c14986a6abb214e6258b6de580234ee07432b43e
kudeh <kene.udeh@gmail.com>
2023-09-09 17:09:43 -0400

    update scripts

--- spark-streaming-pipeline/submit-optional-calculate-score.sh
+++ spark-streaming-pipeline/submit-optional-calculate-score.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec -it nd029-c2-apache-spark-and-spark-streaming-starter_spark_1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/workspace/project/starter/sparkpyoptionalriskcalculation.py | tee ../../spark/logs/optional-score.log 
+docker exec -it spark-streaming-pipeline-spark-1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/workspace/sparkpyoptionalriskcalculation.py | tee  /home/workspace/spark/logs/optional-score.log
+ 
